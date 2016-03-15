1f6a3d40537583bbabbf7b8f159f84e788a0c1b7
Marine <marine.legros@u-psud.fr>
2016-03-15 18:15:21 +0100

    Légères modifications page accueil

--- roadTripHelper/app/Views/post/index.php
+++ roadTripHelper/app/Views/post/index.php
@@ -1,10 +1,17 @@
 
 <div class="container" >
 
-		<p style="padding-right:60px; padding-top:25px" ><img src="img/create_banner.php" style="max-width:100%;"></p>
+		<p class="col-lg-11" style="padding-left:45px; padding-top:25px; padding-bottom:25px" ><img src="img/create_banner.php" style="max-width:100%;"></p>
 
 		<div class="container">
 		  <!-- Example row of columns -->
+		   <div class="jumbotron col-lg-11">
+			<div class="container">
+				<h1>Rejoignez les RoadTripers</h1>
+				<p class="lead">Partagez vos expériences avec les autres voyageurs en vous créant un compte. Commentez les anectodes des autres et partagez sur vos réseaux sociaux favoris.</p>
+				<p><a class="btn btn-lg btn-success" href="#" role="button">Se créer un compte</a></p>
+			</div>
+		  </div>
 		  <div class="row">
 			<div class="col-md-5">
 			  <h2 style="padding-left:40px">Organiser un RoadTrip</h2>
@@ -12,7 +19,7 @@
 			  <p style="padding-left:40px"><a class="btn btn-default" href="#" role="button">J'organise mon RoadTrip &raquo;</a></p>
 			</div>
 			<div class="col-md-5">
-			  <h2 style="padding-left:50px">Heading</h2>
+			  <h2 style="padding-left:50px">Expériences</h2>
 			  <p style="padding-left:50px">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 			  <p style="padding-left:50px"><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 		   </div>
